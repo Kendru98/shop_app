b62a5d82075c0e9594a0830d978f213130586bdb
Kendruu <kendru1998@gmail.com>
2022-06-15 15:25:01 +0200

    First commit

---
+++ lib/screens/shopmainscreen.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class ShopHomePage extends StatelessWidget {
+  const ShopHomePage({Key? key, required String title}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
